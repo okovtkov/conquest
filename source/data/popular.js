5a4dc832d23464d99c44c20a0cf7bc541b885aed
Oleg <oleg_kovtkov@mail.ru>
2021-05-19 21:31:18 +0300

    сверстал популярные товары popular и карточку popular-watch

---
+++ source/data/popular.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let popular = [
+    {
+        name: 'RADO',
+        price: 65300,
+        image: '/img/main_watch_1.png',
+        mod: 'rado-1'
+    },
+    {
+        name: 'RADO',
+        price: 65300,
+        image: '/img/main_watch_2.png',
+        mod: 'rado-2'
+    },
+    {
+        name: 'BVLGARI',
+        price: 81700,
+        image: '/img/main_watch_3.png',
+        mod: 'bvlgari-2'
+    },
+    {
+        name: 'BVLGARI',
+        price: 72700,
+        image: '/img/main_watch_4.png',
+        mod: 'bvlgari-2'
+    }
+];
+
+module.exports = popular;
