51dc52a12bc7088f13cc6e80de103169b1592235
Oleg <oleg_kovtkov@mail.ru>
2021-05-27 02:01:45 +0300

    адаптировал фильтр

---
+++ source/components/filter/filter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let button = document.querySelector('.filter__button');
+let filter = document.querySelector('.filter__form');
+
+button.addEventListener('click', () => {
+    filter.classList.toggle('filter__form_active');
+});
+
