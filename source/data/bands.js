80d51518ea487742ad0484efc4ec827886570d72
Oleg <oleg_kovtkov@mail.ru>
2021-06-01 02:13:28 +0300

    добавил bread-crumbs и slider в catalog. Добавил ссылки в ранее сделанные компоненты

---
+++ source/data/bands.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let bands = [
+    {
+        name: 'Georg Jensen',
+        price: 6500,
+        image: '/img/band_1.png'
+    },
+    {
+        name: 'Nato',
+        price: 3200,
+        image: '/img/band_2.png'
+    },
+    {
+        name: 'Spark',
+        price: 5400,
+        image: '/img/band_3.png'
+    }
+];
+
+module.exports = bands;
