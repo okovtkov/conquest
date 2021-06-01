517a5ee088d610484f87889910904f976474c5b2
Oleg <oleg_kovtkov@mail.ru>
2021-06-01 03:12:05 +0300

    сделал визуально рабочим селект в каталоге

--- source/components/select/select.js
+++ source/components/select/select.js
@@ -1,2 +1,13 @@
 'use strict';
 
+let select = document.querySelector('.select');
+let active = document.querySelector('.select__option_active');
+let options = document.querySelector('.select__options');
+
+select.addEventListener('click', () => {
+    options.classList.toggle('select__options_active');
+});
+
+options.addEventListener('click', (event) => {
+    active.textContent = event.target.textContent;
+});
